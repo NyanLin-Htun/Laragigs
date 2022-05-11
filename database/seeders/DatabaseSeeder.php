eaba1798e8b72a8c435c39144bb0828daeec11f8
NYAN-LIN-htun1311 <nlinhtun1314@gmail.com>
2022-05-11 18:45:46 +0630

    Separating Components/fetch data from db for index

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Listing;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();
+        \App\Models\User::factory(10)->create();
+
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+        // Listing::create([
+        //     'title' => 'Laravel Senior Developer',
+        //     'tags' => 'laravel,javascript',
+        //     'company' => 'Acme Crop',
+        //     'location' => 'Boston, MA',
+        //     'email' => 'email1@email.com',
+        //     'website' => 'https://www.acme.com',
+        //     'description' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque deleniti, aut non optio veritatis reiciendis tempora, alias soluta in accusantium iure autem perspiciatis odit id quibusdam incidunt rerum eius hic.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque deleniti, aut non optio veritatis reiciendis tempora, alias soluta in accusantium iure autem perspiciatis odit id quibusdam incidunt rerum eius hic.'
+        // ]);
+        // Listing::create([
+        //     'title' => 'React Senior Developer',
+        //     'tags' => 'react,javascript',
+        //     'company' => 'Acme Crop',
+        //     'location' => 'Boston, MA',
+        //     'email' => 'email2@email.com',
+        //     'website' => 'https://www.acme.com',
+        //     'description' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque deleniti, aut non optio veritatis reiciendis tempora, alias soluta in accusantium iure autem perspiciatis odit id quibusdam incidunt rerum eius hic.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque deleniti, aut non optio veritatis reiciendis tempora, alias soluta in accusantium iure autem perspiciatis odit id quibusdam incidunt rerum eius hic.'
+        // ]);
+        Listing::factory(6)->create();
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+        <x-Hero/>
+
+        <main>
+        <x-search/>
+            <x-listing-section :listings="$listings"/>
+        </main>
+
+       <x-footer/>
+</x-layout>
+    
