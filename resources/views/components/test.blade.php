f02695ab70ef1a98cad690b87c82d9893d24a26b
NyanLin-Htun <nyanlinhtun147000@gmail.com>
2022-05-18 23:57:31 +0630

    Test blade

---
+++ resources/views/components/test.blade.php
@@ -0,0 +1,22 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <table class="table table-hover">
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>Nyan</td>
+                            <td>nyan@123gmail.com</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
